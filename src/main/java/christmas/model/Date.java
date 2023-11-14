8e0a98a563cf59eca2daad7647f4b27bf151ffe9
sunwooiljoon <wnsdl1659@naver.com>
2023-11-15 00:14:04 +0900

    feat : 크리스마스 디데이 할인 계산 구현
    - getChristmasDDayDiscount 함수 구현

--- src/main/java/christmas/model/Date.java
+++ src/main/java/christmas/model/Date.java
@@ -12,6 +12,13 @@ public class Date {
     }
 
     // 크리스마스 디데이 할인
+    public Integer getChristmasDDayDiscount() {
+        if (date >= 25) {
+            return 3400;
+        }
+        return 1000 + (date - 1) * 100;
+    }
+
     // 평일 할인
     // 주말 할인
     // 특별 할인
