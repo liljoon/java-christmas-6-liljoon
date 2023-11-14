00dc07995a9278574cf72f59c2d30d01fb8f374f
sunwooiljoon <wnsdl1659@naver.com>
2023-11-14 17:35:34 +0900

    feat : 방문 날짜 입력 구현
    - readDate() 함수 구현
    - 문자가 들어오거나 1이상 31이하가 아닐 때 예외 발생

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,5 +3,16 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    
+    public int readDate() {
+        int date;
+
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = Console.readLine();
+        date = Integer.parseInt(input);
+
+        if (date < 1 || date > 31)
+            throw new IllegalArgumentException();
+
+        return date;
+    }
 }
